6004d13ec8a938e2c81c6fb7eac5982a481c1052
Thibaud Myotte <thibaud@Thibauds-MacBook-Air.local>
2024-07-23 19:51:23 +0200

    mino test

--- script_test.py
+++ script_test.py
@@ -2,4 +2,4 @@ with open('story.txt', 'r') as file:
     content = file.read()
     print(content)
 
-print('Est-ce que ce script fonctionne? et n\'est que sur la branche test?')
+print('C\'est que sur la branche minbo ?')
